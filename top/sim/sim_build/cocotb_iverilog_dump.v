0221220be1e349f94ea71514a0e78883442c0a8a
Nathan Mustafa <natemustafanam@gmail.com>
2024-11-19 17:03:36 -0500

    Remove ignored files

--- top/sim/sim_build/cocotb_iverilog_dump.v
+++
@@ -1,6 +0,0 @@
-module cocotb_iverilog_dump();
-initial begin
-    $dumpfile("/home/nathan/holeInTheWall-62050/top/sim/sim_build/wall_bit_mask.fst");
-    $dumpvars(0, wall_bit_mask);
-end
-endmodule
